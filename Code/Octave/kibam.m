54cb3458d5c599d7c23ce19c6d427ca38b78d6f1
Leomr85 <leomr85@gmail.com>
2014-04-29 13:18:14 -0300

    Add Multi-battery concept.

--- Code/Octave/kibam.m
+++ Code/Octave/kibam.m
@@ -15,19 +15,19 @@ function [y0, i0, j0, t0] = kibam (c, k, y0, i0, j0, t0, task_i, task_t, fid)
 	
 	% Defining variables.
 	I = task_i;
+	t = task_t;
 	y0 = i0 + j0;
 	
 	% Calculating k' (k_ in this case) as described in [Jongerden - Battery Model].
 	k_ = k / (c * (1-c));
 	
-	% Calculating the available charge on the wells.
-	t = (task_t);
 	t0 = t0 + (task_t);
 	
+	% Calculating the available charge on the wells.
 	i = (i0 * exp(- k_ * t)) + ((((y0 * k_ * c) - I)*(1 - exp(- k_ * t))) / k_) - (I * c * ((k_ * t) - 1 + exp(- k_ * t)) / k_);
-	fprintf(fid, "%f %f\n", t0/60, i);	% This line only writes the Available Charge Well state.
 	j = (j0 * exp(- k_ * t)) + (y0 * (1 - c) * (1 - exp(- k_ * t))) - ((I * (1 - c) * ((k_ * t) - 1 + exp(- k_ * t))) / k_);
 
+	% fprintf(fid, "%f %f\n", t0/60, i);	% This line only writes the Available Charge Well state.
 	% If you intend to use the following line, please comment the previous 'fprintf' statement.
 	% fprintf(fid, "%f %f %f\n", t, i, j);	% This line writes the Available and Bound Charge Well state.
 
